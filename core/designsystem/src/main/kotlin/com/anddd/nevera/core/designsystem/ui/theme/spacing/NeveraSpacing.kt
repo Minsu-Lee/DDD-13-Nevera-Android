2b4a20861e70a462883cb25e9924f48d401ee607
Ju Hyeok <zero5.two4@gmail.com>
2026-05-04 20:19:03 +0900

    refactor(designsystem): spacing 토큰 이름을 dp 값 기반으로 변경

--- core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/ui/theme/spacing/NeveraSpacing.kt
+++ core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/ui/theme/spacing/NeveraSpacing.kt
@@ -6,26 +6,27 @@ import androidx.compose.ui.unit.dp
 object NeveraSpacing {
 
     // gap
-    val gap0:  Dp = 0.dp
-    val gap1:  Dp = 2.dp
-    val gap2:  Dp = 4.dp
-    val gap3:  Dp = 6.dp
-    val gap4:  Dp = 8.dp
-    val gap5:  Dp = 12.dp
-    val gap6:  Dp = 16.dp
-    val gap7:  Dp = 20.dp
-    val gap8:  Dp = 24.dp
-    val gap9:  Dp = 32.dp
-    val gap10: Dp = 40.dp
+    val gap0: Dp = 0.dp
+    val gap2: Dp = 2.dp
+    val gap4: Dp = 4.dp
+    val gap6: Dp = 6.dp
+    val gap8: Dp = 8.dp
+    val gap12: Dp = 12.dp
+    val gap16: Dp = 16.dp
+    val gap20: Dp = 20.dp
+    val gap24: Dp = 24.dp
+    val gap32: Dp = 32.dp
+    val gap40: Dp = 40.dp
 
     // padding
-    val padding1: Dp = 4.dp
-    val padding2: Dp = 6.dp
-    val padding3: Dp = 8.dp
-    val padding4: Dp = 10.dp
-    val padding5: Dp = 12.dp
-    val padding6: Dp = 16.dp
-    val padding7: Dp = 20.dp
-    val padding8: Dp = 24.dp
-    val padding9: Dp = 32.dp
+    val padding4: Dp = 4.dp
+    val padding6: Dp = 6.dp
+    val padding8: Dp = 8.dp
+    val padding10: Dp = 10.dp
+    val padding12: Dp = 12.dp
+    val padding16: Dp = 16.dp
+    val padding20: Dp = 20.dp
+    val padding24: Dp = 24.dp
+    val padding32: Dp = 32.dp
+    val padding40: Dp = 40.dp
 }
