9e35d573bbb91367c3266ff31a4b4967ebe7478b
jackson <mns0304@gmail.com>
2026-05-19 22:03:45 +0900

    refactor(auth): 에러 코드 매직 넘버를 네임드 상수로 추출

--- data/src/main/kotlin/com/anddd/nevera/data/mapper/error/EmailRequestErrorMapper.kt
+++ data/src/main/kotlin/com/anddd/nevera/data/mapper/error/EmailRequestErrorMapper.kt
@@ -3,10 +3,15 @@ package com.anddd.nevera.data.mapper.error
 import com.anddd.nevera.core.common.NetworkError
 import com.anddd.nevera.domain.model.auth.EmailRequestError
 
+private object EmailRequestErrorCode {
+    const val DUPLICATE_EMAIL = 2006
+    const val MAIL_SEND_ERROR = 2007
+}
+
 internal fun NetworkError.toEmailRequestError(): EmailRequestError = when (this) {
     is NetworkError.HttpError -> when (code) {
-        2006 -> EmailRequestError.DuplicateEmail(serverMessage = message)
-        2007 -> EmailRequestError.MailSendError(serverMessage = message)
+        EmailRequestErrorCode.DUPLICATE_EMAIL -> EmailRequestError.DuplicateEmail(serverMessage = message)
+        EmailRequestErrorCode.MAIL_SEND_ERROR -> EmailRequestError.MailSendError(serverMessage = message)
         else -> EmailRequestError.Common(toCommonError())
     }
     else -> EmailRequestError.Common(toCommonError())
