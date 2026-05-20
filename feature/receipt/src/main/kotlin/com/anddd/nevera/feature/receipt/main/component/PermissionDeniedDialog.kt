cd58b93d1e21ca4f01248bd25d36d751965bc068
jackson <mns0304@gmail.com>
2026-05-20 19:38:14 +0900

    feat(receipt): PermissionDeniedDialog @Preview 추가

--- feature/receipt/src/main/kotlin/com/anddd/nevera/feature/receipt/main/component/PermissionDeniedDialog.kt
+++ feature/receipt/src/main/kotlin/com/anddd/nevera/feature/receipt/main/component/PermissionDeniedDialog.kt
@@ -2,8 +2,10 @@ package com.anddd.nevera.feature.receipt.main.component
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.anddd.nevera.core.designsystem.component.button.NeveraButtonColor
 import com.anddd.nevera.core.designsystem.component.dialog.NeveraConfirmDialog
+import com.anddd.nevera.core.designsystem.ui.theme.NeveraTheme
 import com.anddd.nevera.feature.receipt.R
 
 @Composable
@@ -21,4 +23,19 @@ internal fun PermissionDeniedDialog(
         positiveButtonColor = NeveraButtonColor.Primary,
         negativeButtonColor = NeveraButtonColor.Secondary,
     )
+}
+
+@Preview(
+    name = "PermissionDeniedDialog",
+    showBackground = true,
+    widthDp = 360,
+)
+@Composable
+private fun PermissionDeniedDialogPreview() {
+    NeveraTheme {
+        PermissionDeniedDialog(
+            onDismiss = {},
+            onOpenSettings = {},
+        )
+    }
 }
