80e4c6eca86343b87405c12310ef8b193c8927d2
jackson <mns0304@gmail.com>
2026-05-20 19:37:38 +0900

    fix(receipt): MainActivity 미사용 ORDER_CAPTURE_ROUTE import 제거

--- app/src/main/kotlin/com/anddd/nevera/MainActivity.kt
+++ app/src/main/kotlin/com/anddd/nevera/MainActivity.kt
@@ -25,7 +25,6 @@ import com.anddd.nevera.feature.mypage.main.navigation.MY_PAGE_ROUTE
 import com.anddd.nevera.feature.mypage.main.navigation.myPageScreen
 import com.anddd.nevera.feature.mypage.settingaccount.navigation.SETTING_ACCOUNT_ROUTE
 import com.anddd.nevera.feature.mypage.settingaccount.navigation.settingAccountScreen
-import com.anddd.nevera.feature.receipt.main.navigation.ORDER_CAPTURE_ROUTE
 import com.anddd.nevera.feature.receipt.main.navigation.RECEIPT_ROUTE
 import com.anddd.nevera.feature.receipt.main.navigation.receiptScreen
 import dagger.hilt.android.AndroidEntryPoint
